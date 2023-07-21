0704b98d925d790a42571840990c56570c11994a
SuviSiva <rsuvisivadme2000@gmail.com>
2023-07-21 18:13:11 +0530

    second commit

--- src/app1/App1.java
+++ src/app1/App1.java
@@ -3,16 +3,16 @@ package app1;
 
 public class App1 {
 
-    public static void add(int x ,int y)
+    public static void sub(int x ,int y)
     {
-        System.out.println(x+y);
+        System.out.println(x-y);
     }
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        add(5,7);
+        System.out.println("Hello World this is nice line");
+        sub(5,7);
         New c1 = new New();
         c1.rev("Suvi Siva");
-
+        System.out.println("This is working first ");
     }
     
 }
