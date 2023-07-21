4dd8ee1db6663b3c5354423673d6cfa7fc19475d
Suvi001 <139204294+Suvi001@users.noreply.github.com>
2023-07-21 19:01:15 +0530

    Update App1.java
    suvi

--- src/app1/App1.java
+++ src/app1/App1.java
@@ -3,15 +3,15 @@ package app1;
 
 public class App1 {
 
-    public static void add(int x ,int y)
+    public static void sub(int x ,int y)
     {
-        System.out.println(x+y);
+        System.out.println(x-y);
     }
     public static void main(String[] args) {
         System.out.println("Hello World");
-        add(5,7);
+        sub(5,7);
         New c1 = new New();
-        c1.rev("Suvi Siva");
+        c1.rev("Raja");
 
     }
     
